186a7f5110583353af734188a6e64c5127d5ac50
jaroslawwielgus <jaroslaw.wielgus7557@gmail.com>
2021-12-28 18:06:20 +0100

    Update6

--- app/src/main/java/com/example/portfolio4/SearchingFragment.kt
+++ app/src/main/java/com/example/portfolio4/SearchingFragment.kt
@@ -3,6 +3,7 @@ package com.example.portfolio4
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -12,6 +13,14 @@ import com.example.portfolio4.databinding.FragmentSearchingBinding
 class SearchingFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding: FragmentSearchingBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_searching, container, false)
+
+        binding.meButton.setOnClickListener { v: View ->
+            v.findNavController().navigate(SearchingFragmentDirections.actionSearchingFragmentToProfileFragment())
+        }
+
+        binding.workerName1.setOnClickListener { v: View ->
+            v.findNavController().navigate(SearchingFragmentDirections.actionSearchingFragmentToProfileFragment())
+        }
 
         return binding.root
     }

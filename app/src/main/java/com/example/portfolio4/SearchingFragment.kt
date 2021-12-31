a95c88d113492d7a21e579ab7f7a384f474e452f
jaroslawwielgus <jaroslaw.wielgus7557@gmail.com>
2021-12-31 12:07:00 +0100

    Update12-users

--- app/src/main/java/com/example/portfolio4/SearchingFragment.kt
+++ app/src/main/java/com/example/portfolio4/SearchingFragment.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.portfolio4.databinding.FragmentSearchingBinding
+import org.w3c.dom.Text
 
 class SearchingFragment: Fragment() {
     val usersList: List<User>? = MainActivity.usersList
@@ -28,6 +32,55 @@ class SearchingFragment: Fragment() {
             v.findNavController().navigate(SearchingFragmentDirections.actionSearchingFragmentToProfileFragment())
         }
 
+        binding.searchButton.setOnClickListener {v: View ->
+            if (usersList != null) {
+                var isFound: Boolean = false
+                val searchedUser: String = binding.searchingText.text.toString()
+                //var foundUsers: MutableList<User> = MutableList<User>(4)
+                for (i in usersList) {
+                    if (i.name + " " + i.surname == searchedUser) {
+                        isFound = true
+                        //foundUsers.add(i)
+                        //create button
+                        val workerButton = Button(context)
+                        val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+                        workerButton.layoutParams = params
+                        workerButton.text = searchedUser.uppercase()
+                        workerButton.textAlignment = View.TEXT_ALIGNMENT_CENTER
+                        workerButton.setPadding(0, 16 , 0, 16)
+
+                        //create listener
+                        binding.apply {
+                            layout1.addView(workerButton)
+
+                            workerButton.setOnClickListener {
+                                chosenUser = i
+                                loggedUser = MainActivity.user!!
+                                MainActivity.user = chosenUser
+
+                                v.findNavController().navigate(SearchingFragmentDirections.actionSearchingFragmentToProfileFragment())
+                                MainActivity.user = loggedUser
+                            }
+                        }
+
+                    }
+                }
+                if (!isFound) {
+                    val errorView = TextView(context)
+                    val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+                    errorView.layoutParams = params
+                    errorView.text = "Nie znaleziono pracownika"
+                    errorView.textAlignment = View.TEXT_ALIGNMENT_CENTER
+                    errorView.setPadding(0, 16 , 0, 16)
+
+                    binding.apply {
+                        layout1.addView(errorView)
+                    }
+                }
+            }
+        }
+
+        /*
         binding.workerName1.setOnClickListener { v: View ->
             /*
             if (usersList != null) {
@@ -46,6 +99,9 @@ class SearchingFragment: Fragment() {
             v.findNavController().navigate(SearchingFragmentDirections.actionSearchingFragmentToProfileFragment())
             MainActivity.user = loggedUser
         }
+         */
+
+
 
         return binding.root
     }
